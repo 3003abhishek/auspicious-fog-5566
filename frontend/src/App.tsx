a02f6e75f2790b71b4c34c81f01d182a13162f42
unknown <3003abhishek.jha@gmail.comgit config --global user.name Abhishek>
2023-03-03 01:06:14 +0530

    initial layout of players button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import { useState } from 'react'
 import './App.css'
-
+import Home from './Pages/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Box className="App">
-      
+      <Home/>
     </Box>
   )
 }
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
-
+import { Box } from '@chakra-ui/react';
+import Players from '../Components/Home/player';
+import { useState } from 'react';
 const Home = () => {
+  const [player,setPlayer]=useState("")
+  let handlePlayer=(propValue:string):void=>{
+     setPlayer(propValue);
+     
+  }
+
+  console.log(player);
+
   return (
+    <>
+    
     <Box>
         Home Page
+
     </Box>
+    
+    <Players onPropChange={handlePlayer}/>
+    </>
+    
   )
 }
 
