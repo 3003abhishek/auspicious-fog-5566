7af0af94c85891ad4d81d38ff1f9b3f5eedcc71c
Suneet25 <suneetpanigrahi2505@gmail.com>
2023-03-03 09:37:59 +0530

    added day-1 task

--- frontend/src/Components/Home/GameOptions.tsx
+++ frontend/src/Components/Home/GameOptions.tsx
@@ -1,7 +1,40 @@
-import { Box, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Modal,
+  Input,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  useDisclosure,
+  Select,
+  Flex,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
-
+type Props = {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+};
 const GameOptions = () => {
+  let [player, setPlayer] = React.useState<string>("");
+  let [mode, setMode] = React.useState<string>("");
+  let [single, setSingle] = React.useState<string>("");
+  let [multi, setMulti] = React.useState<string>("");
+  let [player1, setPlayer1] = React.useState<string>("");
+  let [player2, setPlayer2] = React.useState<string>("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  console.log(mode);
+  console.log(player1);
+  console.log(player2);
   return (
     <Box
       w={"25%"}
@@ -12,12 +45,75 @@ const GameOptions = () => {
       borderRadius={"0.4rem"}
       boxShadow="rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
     >
-      <Button colorScheme='yellow' variant="solid" mb={"2rem"} size={"lg"}>
+      <Button
+        colorScheme="yellow"
+        variant="solid"
+        mb={"2rem"}
+        size={"lg"}
+        onClick={onOpen}
+      >
         New Game
       </Button>
-      <Button colorScheme='yellow' variant="solid" size={"lg"}>
+      <Button colorScheme="yellow" variant="solid" size={"lg"}>
         Score Board
       </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={5}>
+              <FormControl>
+                <FormLabel>Choose to play 🙂</FormLabel>
+                <Select onChange={(e) => setMode(e.target.value)}>
+                  <option>Select Mode</option>
+                  <option value="single">Single Player</option>
+                  <option value="multi">Multi Player</option>
+                </Select>
+                {mode === "single" ? (
+                  <Box>
+                    <FormLabel mt={6}>Player1 Name</FormLabel>
+                    <Input
+                      type="text"
+                      placeholder="Add Playername..."
+                      value={player1}
+                      onChange={(e) => setPlayer1(e.target.value)}
+                      mt={3}
+                    />
+                  </Box>
+                ) : (
+                  <Box>
+                    <FormLabel mt={6}>Player1 Name</FormLabel>
+                    <Input
+                      type="text"
+                      placeholder="Add Playername..."
+                      value={player1}
+                      onChange={(e) => setPlayer1(e.target.value)}
+                      mt={3}
+                    />
+
+                    <FormLabel mt={6}>Player2 Name</FormLabel>
+                    <Input
+                      type="text"
+                      placeholder="Add Playername..."
+                      value={player2}
+                      onChange={(e) => setPlayer2(e.target.value)}
+                      mt={3}
+                    />
+                  </Box>
+                )}
+              </FormControl>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
